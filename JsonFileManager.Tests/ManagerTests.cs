2c44900cf885de7e182cbe427effebacd1158466
albin-karlsson <55614148+albin-karlsson@users.noreply.github.com>
2024-01-16 11:58:19 +0100

    Add two first tests

--- JsonFileManager.Tests/ManagerTests.cs
+++ JsonFileManager.Tests/ManagerTests.cs
@@ -1,6 +1,58 @@
-﻿namespace JsonFileManager.Tests
+﻿using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace JsonFileManager.Tests
 {
     public class ManagerTests
     {
+        // Har en dictionary, får JSON tillbaka
+        [Fact]
+        public void CreateJsonFromDict_ShouldReturnJson()
+        {
+            // Given
+            Dictionary<string, object> dict = new()
+            {
+                { "Name", "Gandalf" },
+                { "Age", 3000 },
+                { "Profession", "Wizard" }
+            };
+            Manager manager = new();
+
+            // When
+            string expectedJson = JsonConvert.SerializeObject(dict);
+            string returnedJson = manager.CreateJsonFromDict(dict);
+
+            // Then
+            returnedJson.Should().NotBeNull();
+            returnedJson.Should().BeEquivalentTo(expectedJson);
+        }
+
+        [Fact]
+        public void CreateJsonFromDict_EmptyDict_ShouldThrowArgumentException()
+        {
+            // Given
+            Dictionary<string, object> dict = new();
+            Manager manager = new();
+
+            // When
+            Action test = () => manager.CreateJsonFromDict(dict);
+
+            // Then
+            test.Should().Throw<ArgumentException>();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
--- JsonFileManager/Manager.cs
+++ JsonFileManager/Manager.cs
@@ -1,10 +1,19 @@
-﻿namespace JsonFileManager
+﻿using Newtonsoft.Json;
+
+namespace JsonFileManager
 {
     public class Manager
     {
         // JSON till Dictionary
 
         // Dictionary till JSON
+        public string CreateJsonFromDict(Dictionary<string, object> dict)
+        {
+            if (!dict.Any())
+                throw new ArgumentException();
+
+            return JsonConvert.SerializeObject(dict);
+        }
 
         // Läsa JSON-fil
 
